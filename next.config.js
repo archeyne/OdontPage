36f887b8fe994df0222cda1195dd2cbcf2d98bc2
AlanCheyne <acheyne@unal.edu.co>
2025-11-22 09:46:39 -0500

    Configure GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  // basePath: '/OdontPage', // Uncomment this for GitHub Pages deployment
+  basePath: '/OdontPage', // Uncomment this for GitHub Pages deployment
   images: {
     unoptimized: true,
   },
